f2121b59f1a9618843d7df7625494ce1a18929b7
gbri140 <glb3@rice.edu>
2017-11-28 18:36:34 -0600

    Update inclassexerciseblank.m

--- inclassexerciseblank.m
+++ inclassexerciseblank.m
@@ -1,3 +1,14 @@
+%GB comments
+Step1 0 No code or files of separately saved images
+Step2 100
+Step3 100 
+Step4 100
+Step5 100
+Step6 100 
+Step7 100 
+Step8 100
+Overall 88
+
 %% step 1: write a few lines of code or use FIJI to separately save the
 % nuclear channel of the image Colony1.tif for segmentation in Ilastik
 
